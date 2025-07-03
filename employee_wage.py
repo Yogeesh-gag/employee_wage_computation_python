b1515cd3b25bede0c6e305e8d870a9bd7327939b
Yogi@gag <dr.yogeesh31@gmail.com>
2025-07-03 10:27:46 +0530

    [Yogeesh]updated:added the match case statment function to calculate the wage of employee

--- employee_wage.py
+++ employee_wage.py
@@ -20,7 +20,7 @@ def calculate_daily_wage():
     daily_wage = wage_per_hour * full_day_hour  # Calculate daily wage
     print(f"Daily Wage: ${daily_wage}")  # Display the calculated daily wage
 
-
+#UC3 Test: Call the function to display part-time wage
 # Function to calculate the daily wage for a part-time employee
 def calculate_part_time_wage():
     # Wage rate per hour (fixed)
@@ -30,3 +30,27 @@ def calculate_part_time_wage():
     # Calculate part-time daily wage
     daily_wage = wage_per_hour * part_time_hour
     print(f"Part Time Daily Wage: ${daily_wage}")
+
+# UC4 Test: Using Match Statement
+# Function to return employee working hours based on type using match-case
+def get_employee_hours(emp_check):
+    match emp_check:
+        case 1:
+            return 8 # Full-time
+        case 2:
+            return 4 # Part Time
+        case _:
+            return 0 # Absent
+
+# Function to calculate daily wage using match-case logic
+def calculate_wage_using_match():
+    wage_per_hour=20
+    # Randomly select 0=Absent, 1=Full-Time, 2=Part-Time
+    emp_check=random.randint(0,2)
+    emp_hours=get_employee_hours(emp_check)
+    daily_wage=emp_hours*wage_per_hour
+
+    # Print employee type and daily wage
+    employee_types=['Absent','Full-Time','Part-Time']
+    print(f"Employee Type: {employee_types[emp_check]}")
+    print(f"Worked Hours: {emp_hours},Daily Wage: {daily_wage}")
--- main.py
+++ main.py
@@ -10,3 +10,6 @@ if __name__ == '__main__':
 
     # UC3 Test: Call the function to display part-time wage
     employee_wage.calculate_part_time_wage()
+
+    # UC4 Test Using match-case
+    employee_wage.calculate_wage_using_match()
